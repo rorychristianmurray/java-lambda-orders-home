a8f2022c2266c59b0069bdaa8249ae03bb48b9f5
Rory Murray <rory.christian.murray@gmail.com>
2019-08-14 16:56:00 -0400

    build CustomerService

--- src/main/java/com/lambdaschool/ordersapp/services/CustomersService.java
+++ src/main/java/com/lambdaschool/ordersapp/services/CustomersService.java
@@ -1,5 +1,9 @@
 package com.lambdaschool.ordersapp.services;
 
+import com.lambdaschool.ordersapp.model.Customers;
+
+import java.util.List;
+
 public interface CustomersService
 {
     // we need to
@@ -9,6 +13,25 @@ public interface CustomersService
     // create a new customer
     // update a customer by id
     // delete a customer by id
+
+    // find all customers will be function of type list with items of the Customers data type
+    List<Customers> findAllCustomers();
+
+    // find customers by id
+    Customers findCustomersById(long id);
+
+    // find customers by name
+    Customers findCustomersByName(String name);
+
+    // create a new customer
+    Customers save(Customers customer);
+
+    // update a customer by id
+    Customers update(Customers customer, long id);
+
+    // delete a customer by id
+    void delete(long id);
+
 
 
 
