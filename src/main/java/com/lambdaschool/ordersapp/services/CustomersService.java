a58d23d6e570d7bd3b2904e9877ec9ae3792cd09
Rory Murray <rory.christian.murray@gmail.com>
2019-08-14 16:47:02 -0400

    pseudo code CustomersService

--- src/main/java/com/lambdaschool/ordersapp/services/CustomersService.java
+++ src/main/java/com/lambdaschool/ordersapp/services/CustomersService.java
@@ -2,4 +2,14 @@ package com.lambdaschool.ordersapp.services;
 
 public interface CustomersService
 {
+    // we need to
+    // find all customers
+    // find customers by id
+    // find customers by name
+    // create a new customer
+    // update a customer by id
+    // delete a customer by id
+
+
+
 }
