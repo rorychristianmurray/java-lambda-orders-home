2bc02e8d9145b21dd6e2835235127fb98e45b21e
Rory Murray <rory.christian.murray@gmail.com>
2019-08-14 22:33:32 -0400

    build OrdersServiceImpl

--- src/main/java/com/lambdaschool/ordersapp/services/OrdersServiceImpl.java
+++ src/main/java/com/lambdaschool/ordersapp/services/OrdersServiceImpl.java
@@ -1,5 +1,15 @@
 package com.lambdaschool.ordersapp.services;
 
-public class OrdersServiceImpl
+import com.lambdaschool.ordersapp.repos.OrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class OrdersServiceImpl implements OrdersService
 {
+    @Autowired
+    private OrdersRepository ordersrepos;
+
 }
