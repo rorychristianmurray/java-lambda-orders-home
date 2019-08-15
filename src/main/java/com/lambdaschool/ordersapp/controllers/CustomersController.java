b14b99ecf9a9d7e28b0e0b0200f9decf7c33fd73
Rory Murray <rory.christian.murray@gmail.com>
2019-08-14 22:55:21 -0400

    build CustomersController

--- src/main/java/com/lambdaschool/ordersapp/controllers/CustomersController.java
+++ src/main/java/com/lambdaschool/ordersapp/controllers/CustomersController.java
@@ -1,5 +1,43 @@
 package com.lambdaschool.ordersapp.controllers;
 
+// let Spring know this is a controller for REST operations
+
+import com.lambdaschool.ordersapp.model.Customers;
+import com.lambdaschool.ordersapp.services.CustomersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer") // optional - sets designated URL
 public class CustomersController
 {
+    @Autowired // some magic, i guess. basically gives us stuff off of CustomersService service
+    private CustomersService customersService;
+
+    // get all customers
+    @GetMapping(value = "/order", produces = {"application/json"}) // tells Spring we want to create endpoint
+    public ResponseEntity<?> listAllCustomers()
+    {
+        List<Customers> theCustomers = customersService.findAllCustomers();
+        return new ResponseEntity<>(theCustomers, HttpStatus.OK)
+    }
+
+    get customer by name
+    @GetMapping(value = "/name/{custname}", produces = {"application/json"})
+    public ResponseEntity<?> getCustomerByName(@PathVariable String custname)
+    {
+        Customers aCustomer = customersService.findCustomersByName(custname);
+        return new ResponseEntity<>(aCustomer, HttpStatus.OK);
+    }
+
+
+
+
 }
